0c7a88059e64ff66f9ed9a31cafcd2bb7c7ca6e7
Arnaud Fournier <arnaudfournier921@gmail.com>
2025-12-23 11:48:05 +0100

    Add tailwind and remove lint

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 import path from 'path';
 
 // https://vite.dev/config/
@@ -14,5 +16,8 @@ export default defineConfig({
       "@pages": path.resolve(__dirname, './src/pages'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
