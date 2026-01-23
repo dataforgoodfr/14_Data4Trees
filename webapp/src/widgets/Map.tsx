4af747c1f80472466266737b91b27e30c9b14982
Arnaud Fournier <arnaudfournier921@gmail.com>
2026-01-23 17:13:21 +0100

    Integrate Coordo module into project

--- webapp/src/widgets/Map.tsx
+++ webapp/src/widgets/Map.tsx
@@ -1,11 +1,20 @@
+// @ts-ignore
+import { createMap } from "coordo"
+import { useEffect } from "react";
 export interface MapProps {
 
 }
 
 export function Map({ }: MapProps) {
+
+    useEffect(() => {
+        createMap("#map");
+    }, []);
+
     return (
-        <div className="flex h-full items-center justify-center p-6">
+        <div className="flex flex-col h-full items-center justify-center p-6">
             <h1 className="py-4 text-center text-foreground font-title-primary">Carte all4Trees</h1>
+            <div id="map" style={{ width: "100%", height: "300px" }}></div>
         </div>
     );
 }
