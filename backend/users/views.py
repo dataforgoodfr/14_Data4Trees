a5783b62d8ba36bcabc9e1b88bd8d436100829a4
Arnaud Fournier <arnaudfournier921@gmail.com>
2026-02-04 11:48:14 +0100

    Change permission to view user and group lists

--- backend/users/views.py
+++ backend/users/views.py
@@ -11,7 +11,7 @@ class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
 
 
 class GroupViewSet(viewsets.ModelViewSet):
@@ -21,4 +21,4 @@ class GroupViewSet(viewsets.ModelViewSet):
 
     queryset = Group.objects.all().order_by("name")
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAdminUser]
