9f8469ff19d9b79df99510e5a60e8dc10f5dcfe2
David Bretaud <david.bretaud.dev@gmail.com>
2026-02-07 17:38:50 +0100

    chore(prettier): Setup prettier (#14)

--- webapp/eslint.config.ts
+++ webapp/eslint.config.ts
@@ -4,6 +4,7 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
+import pluginPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
@@ -27,4 +28,5 @@ export default defineConfig([
   { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
   /** @ts-expect-error json plugin not typed well*/
   { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  pluginPrettier,
 ]);
