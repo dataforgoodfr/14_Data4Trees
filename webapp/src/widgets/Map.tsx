2b19c336726ab988662a8502f723c43ac755cd46
Yannis S <58774206+yannix13@users.noreply.github.com>
2026-02-11 17:11:22 +0100

    chore: affichage carte specs json coordo (#31)
    Co-authored-by: Arnaud Fournier <arnaudfournier921@gmail.com>

--- webapp/src/widgets/Map.tsx
+++ webapp/src/widgets/Map.tsx
@@ -1,48 +1,32 @@
 // @ts-expect-error Could not find a declaration file for module 'coordo'.
 import { createMap } from "coordo";
-import { type FC, useEffect, useRef, useState } from "react";
+import { type FC, useEffect, useRef } from "react";
 
-export type MapApi = {
-  getDataForLayer: (layerId: string) => unknown;
-};
+import style from "@/fixtures/style.json";
 
-// Hook personnalisé pour initialiser la carte
+import "./Map.css";
+
 function useMap(containerSelector: string) {
-  const [mapApi, setMapApi] = useState<MapApi>({} as MapApi);
   const isInitialized = useRef(false);
 
   useEffect(() => {
     if (isInitialized.current) return;
-
     try {
-      const api = createMap(containerSelector);
-      setMapApi(api);
+      createMap(containerSelector, style);
       isInitialized.current = true;
     } catch (error) {
       console.error("Erreur lors de l'initialisation de la carte:", error);
     }
   }, [containerSelector]);
-
-  return mapApi;
 }
 
 export const Map: FC = () => {
-  const mapApi = useMap("#map");
+  useMap("#map");
 
   return (
     <div
       id="map"
       className="w-full h-screen"
-      onClick={() => console.log(mapApi.getDataForLayer("my-layer"))}
-    >
-      <button
-        className="absolute top-4 left-4 z-10 p-2 bg-foreground rounded shadow"
-        onClick={(e) => {
-          e.stopPropagation();
-        }}
-      >
-        Button
-      </button>
-    </div>
+    ></div>
   );
 };
