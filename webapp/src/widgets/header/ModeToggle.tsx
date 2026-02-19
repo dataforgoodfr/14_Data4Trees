7dd43ab81a35a8358666599b24cce0f96bd93e4d
David Bretaud <david.bretaud.dev@gmail.com>
2026-02-20 00:21:33 +0100

    fix(color-mode): Add system variant to ui

--- webapp/src/widgets/header/ModeToggle.tsx
+++ webapp/src/widgets/header/ModeToggle.tsx
@@ -1,6 +1,11 @@
-import { Moon, Sun } from "lucide-react";
+import { cx } from "class-variance-authority";
+import { MonitorCog, Moon, Sun } from "lucide-react";
+import type { FC, ReactNode } from "react";
 
+import type { Theme } from "@shared/contexts/ThemeContext";
 import { useTheme } from "@shared/hooks/useTheme";
+
+import { useTranslation } from "@i18n";
 
 import { Button } from "@ui/button";
 import {
@@ -10,8 +15,46 @@ import {
   DropdownMenuTrigger,
 } from "@ui/dropdown-menu";
 
-export function ModeToggle() {
-  const { setTheme } = useTheme();
+export const ModeToggle: FC = () => {
+  const { setTheme, theme } = useTheme();
+  const { t } = useTranslation("translations");
+
+  const colorModes: Array<{
+    identifier: Theme;
+    icon: (className?: string) => ReactNode;
+    label: string;
+  }> = [
+    {
+      icon: (className) => (
+        <Sun
+          key="icon-light"
+          className={className ?? ""}
+        />
+      ),
+      identifier: "light",
+      label: t("header.colorModeSelector.light"),
+    },
+    {
+      icon: (className) => (
+        <Moon
+          key="icon-dark"
+          className={className ?? ""}
+        />
+      ),
+      identifier: "dark",
+      label: t("header.colorModeSelector.dark"),
+    },
+    {
+      icon: (className) => (
+        <MonitorCog
+          key="icon-system"
+          className={className ?? ""}
+        />
+      ),
+      identifier: "system",
+      label: t("header.colorModeSelector.system"),
+    },
+  ];
 
   return (
     <DropdownMenu>
@@ -21,25 +64,35 @@ export function ModeToggle() {
           size="icon"
           className="focus-visible:ring-0 focus-visible:ring-offset-0 shadow-none"
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <div className="h-[16px] w-[16px] relative">
+            {colorModes.map((config) =>
+              config.icon(
+                cx("transition-all absolute", {
+                  "scale-0 rotate-90": config.identifier !== theme,
+                  "scale-100 rotate-0": config.identifier === theme,
+                }),
+              ),
+            )}
+          </div>
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
+
       <DropdownMenuContent
-        className="bg-background"
+        className="bg-background "
         align="end"
       >
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
+        {colorModes.map((config) => (
+          <DropdownMenuItem
+            key={config.identifier}
+            onClick={() => setTheme(config.identifier)}
+            className="gap-sm"
+          >
+            {config.icon()}
+            {config.label}
+          </DropdownMenuItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
