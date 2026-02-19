a348646caa65f80d9441eb582cb98324f403b91c
David Bretaud <david.bretaud.dev@gmail.com>
2026-02-19 19:05:51 +0100

    fix(tailwind): Fix theme mode selector

--- webapp/src/widgets/header/ModeToggle.tsx
+++ webapp/src/widgets/header/ModeToggle.tsx
@@ -36,6 +36,9 @@ export function ModeToggle() {
         <DropdownMenuItem onClick={() => setTheme("dark")}>
           Dark
         </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
