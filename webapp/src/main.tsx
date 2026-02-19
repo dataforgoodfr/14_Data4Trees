3171e97c334bd3154b7d2c1fd145b32bdf6500e5
David Bretaud <david.bretaud.dev@gmail.com>
2026-02-19 19:21:20 +0100

    fix(tailwind): Remove useless css and have better separation

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 
 import App from "./app/App.tsx";
+import "./index.css";
 import { createRoot } from "react-dom/client";
 
 // biome-ignore lint/style/noNonNullAssertion: <Common Vite way>
