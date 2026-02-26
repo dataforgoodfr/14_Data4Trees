dce246432cf36962f778bae2ff4a011f694314fa
Célestin Besson <celestin.besson@proton.me>
2026-02-26 21:43:12 +0100

    fix: adding mapSettings, setMapSettings in useEffect

--- webapp/src/shared/hooks/use-local-storage.tsx
+++ webapp/src/shared/hooks/use-local-storage.tsx
@@ -6,24 +6,24 @@ type SetValue<T> = T | ((val: T) => T);
  * Hook to manage a value in localStorage.
  * It returns the current value and a function to update it.
  * The value is stored as JSON in localStorage.
- * 
+ *
  * Example usage:
  * ```
  * const [name, setName] = useLocalStorage('user-name', 'Anonymous');
  * ```
- * 
+ *
  * ```
  * const [settings, setSettings] = useLocalStorage<UserSettings>('user-settings', {
  *   theme: 'light',
  *   notifications: true
  * });
  * ```
- * 
+ *
  * @link https://www.usehooks.io/docs/use-local-storage
  */
 export function useLocalStorage<T>(
   key: string,
-  initialValue: T
+  initialValue: T,
 ): [T, (value: SetValue<T>) => void] {
   const [storedValue, setStoredValue] = useState<T>(() => {
     try {
