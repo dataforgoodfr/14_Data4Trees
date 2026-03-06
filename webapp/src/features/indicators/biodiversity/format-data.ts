0115037ce0c884dc55ebe9589227cc83e0567c42
David Bretaud <david.bretaud.dev@gmail.com>
2026-03-06 23:58:01 +0100

    feat(indicator): Add a util hook to internationalized raw data with units

--- webapp/src/features/indicators/biodiversity/format-data.ts
+++ webapp/src/features/indicators/biodiversity/format-data.ts
@@ -1,31 +1,52 @@
-import { i18nInstance } from "@shared/i18n"
+import { i18nInstance } from "@shared/i18n";
+
+import { UNITS, useFormatterWithUnit } from "../utils";
+
+export type BiodiversityData = {
+  for: string;
+  richness: number;
+  dominant_height: number;
+};
 
 /**
- * @todo Inject raw data from JSON
- * Return data in a flatten way for UI rendering
+ * Return data in a convenient way for UI rendering, handling units and fixing
  */
-export function formatBiodiversityData() {
+export const useFormatBiodiversityData = (data: BiodiversityData) => {
+  const { formatWithUnit } = useFormatterWithUnit();
+
   return {
-    title: "Point #se-4",
-    date: Intl.DateTimeFormat(i18nInstance.language, { "dateStyle": "short" }).format(new Date()),
+    title: "Point #se-4", // to replace
+    date: Intl.DateTimeFormat(i18nInstance.language, {
+      dateStyle: "short",
+    }).format(new Date()), // to replace
     biomass: {
-      volume: "5 L",
-      density: "1765"
+      volume: formatWithUnit(5, UNITS.tonPerHectare), // replace hardcoded value
+      density: formatWithUnit(120, UNITS.individualPerHectare), // replace hardcoded value
     },
     treeDiversity: {
-      specificWealth: "Blablabla",
-      shannon: 1.1,
+      specificWealth: formatWithUnit(1257, UNITS.speciesCount), // replace hardcoded value
+      shannon: 1.1, // replace hardcoded value
     },
+    // replace hardcoded value
+    indicatorSpecies: {
+      abundanceTaxon1: 43,
+      specificWealthTaxon1: 47,
+      abundanceTaxon2: 56,
+      specificWealthTaxon2: 23,
+      abundanceTaxon3: 33,
+      specificWealthTaxon3: 24,
+    },
+    // replace hardcoded value
     forestPotentialLevel: {
       benef: {
         density: 70,
         ratioDeathmassBiomass: 85,
-        diversity: 77,
+        diversity: data.richness,
         spatialDistribution: 43,
         diameterDistribution: 22,
         verticalDistribution: 67,
-        masterHeight: 78,
-        microhabitat: 2
+        masterHeight: data.dominant_height,
+        microhabitat: 2,
       },
       temoin: {
         density: 80,
@@ -35,10 +56,10 @@ export function formatBiodiversityData() {
         diameterDistribution: 32,
         verticalDistribution: 67,
         masterHeight: 98,
-        microhabitat: 22
-      }
-    }
-  }
-}
+        microhabitat: 22,
+      },
+    },
+  };
+};
 
-export type FormattedData = ReturnType<typeof formatBiodiversityData>;
+export type FormattedData = ReturnType<typeof useFormatBiodiversityData>;
