03d6cf79b3bade29c6b6e2826dc17697b12c6bca
David Bretaud <david.bretaud.dev@gmail.com>
2026-03-17 09:00:02 +0100

    feat(header): Reorganize items for user settings

--- webapp/src/widgets/header/Header.tsx
+++ webapp/src/widgets/header/Header.tsx
@@ -1,20 +1,8 @@
-import { AdminLink } from "@features/admin/AdminLink";
-
-import { useTranslation } from "@shared/i18n";
-
-import { Button } from "@ui/button";
 import { LanguageSelecor } from "./LanguageSelector";
 import { ModeToggle } from "./ModeToggle";
+import { UserMenu } from "./user-menu";
 
-interface HeaderProps {
-  isAuthenticated: boolean;
-  onLogin: () => void;
-  onLogout: () => void;
-}
-
-export function Header({ onLogout, onLogin, isAuthenticated }: HeaderProps) {
-  const { t } = useTranslation("translations");
-
+export function Header() {
   return (
     <header className="bg-background border-b border-border p-3 relative z-40">
       <div className="mx-auto max-w-screen-2xl">
@@ -25,27 +13,10 @@ export function Header({ onLogout, onLogin, isAuthenticated }: HeaderProps) {
             src="/logo_all4trees.png"
           />
 
-          <div className="flex items-center gap-4">
-            <AdminLink />
-            {isAuthenticated ? (
-              <Button
-                onClick={onLogout}
-                size="default"
-                variant="default"
-              >
-                {t("header.button.logout")}
-              </Button>
-            ) : (
-              <Button
-                onClick={onLogin}
-                size="default"
-                variant="default"
-              >
-                {t("header.button.login")}
-              </Button>
-            )}
+          <div className="flex items-center gap-3">
             <ModeToggle />
             <LanguageSelecor />
+            <UserMenu />
           </div>
         </div>
       </div>
