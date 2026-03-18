efbd8e42826639296977d5056ad3ea839b7b56b2
yannix13 <staali.yannis@gmail.com>
2026-03-18 20:07:35 +0100

    feat: implement MapContext with last coordo version

--- backend/maps/views.py
+++ backend/maps/views.py
@@ -1,6 +1,6 @@
 import json
 
-from coordo.config import MapConfig
+from coordo.map import Map as MapConfig
 from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -8,9 +8,14 @@ from django.views.decorators.csrf import csrf_exempt
 config_path = settings.BASE_DIR / "data" / "config.json"
 parser = MapConfig.from_file(config_path)
 
-
+#Quick fix to be able to filter on map but will need to modify coordo/map/datapackage.py 
 def style_json(request):
-    return JsonResponse(parser.to_maplibre(base_url="/api/maps/layers"))
+    style = parser.get_maplibre_style()
+    base_url = request.build_absolute_uri("/api/maps/layers/")
+    for layer in style["layers"]:
+        if "metadata" in layer and "schema" in layer["metadata"]:
+            layer["metadata"]["url"] = f"{base_url}{layer['id']}"
+    return JsonResponse(style)
 
 
 @csrf_exempt
@@ -18,5 +23,5 @@ def map_data(request, layer_id):
     filters = None
     if request.method == "POST" and request.body:
         filters = json.loads(request.body.decode("utf-8"))
-    geojson = parser.get_data(layer_id, filters)
+    geojson = parser.get_layer_data(layer_id, filters)
     return JsonResponse(geojson)
--- webapp/src/app/providers/index.ts
+++ webapp/src/app/providers/index.ts
@@ -1,3 +1,4 @@
 export { ApiProvider } from "./ApiProvider";
 export { AuthProvider } from "./AuthProvider";
+export { MapProvider } from "./MapProvider";
 export { ThemeProvider } from "./ThemeProvider";
