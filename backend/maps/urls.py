74ef0a6d7fe8aa4f073020dafc49d32ccae2ed40
Cabanon <mathias.cabanne@gmail.com>
2026-03-19 18:51:03 +0100

    Fix style.json route

--- backend/maps/urls.py
+++ backend/maps/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("style.json", views.style_json, name="maps-style"),
-    path("layers/<str:layer_id>", views.map_data, name="maps-data"),
+    path("<path:subpath>", views.my_map_view, name="maps-data"),
 ]
--- backend/maps/views.py
+++ backend/maps/views.py
@@ -5,18 +5,16 @@ from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-config_path = settings.BASE_DIR / "data" / "config.json"
-parser = Map.from_file(config_path)
-
-
-def style_json(request):
-    return JsonResponse(parser.get_maplibre_style())
+config_path = settings.BASE_DIR / "configs" / "config.json"
+map = Map.from_file(config_path)
 
 
 @csrf_exempt
-def map_data(request, layer_id):
-    filters = None
-    if request.method == "POST" and request.body:
-        filters = json.loads(request.body.decode("utf-8"))
-    geojson = parser.get_data(layer_id, filters)
-    return JsonResponse(geojson)
+def my_map_view(request, subpath):
+    return JsonResponse(
+        map.handle_request(
+            request.method,
+            subpath,
+            request.body,
+        )
+    )
