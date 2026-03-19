d2aaee91ee705b2a93e6f187c8f6de352974ac6d
David Bretaud <david.bretaud.dev@gmail.com>
2026-03-19 22:17:25 +0100

    refactor(header): Switch to kebab case for file naming

--- webapp/src/widgets/header/index.tsx
+++ webapp/src/widgets/header/index.tsx
@@ -1,1 +1,21 @@
-export { Header } from "./Header";
+import { UserMenu } from "./user-menu";
+
+export function Header() {
+  return (
+    <header className="bg-background border-b border-border p-3 relative z-40">
+      <div className="mx-auto max-w-screen-2xl">
+        <div className="flex items-center justify-between gap-md">
+          <img
+            alt="Logo"
+            className="h-12"
+            src="/logo_all4trees.png"
+          />
+
+          <div className="flex items-center gap-3">
+            <UserMenu />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
