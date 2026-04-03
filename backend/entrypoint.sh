820c142a77f28bf8fdb6ae0879f9742ea476805c
Arnaud Fournier <arnaudfournier921@gmail.com>
2026-04-03 14:31:27 +0200

    Fix docker entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -15,6 +15,8 @@ else
     coordo load file data/dens_bois.csv --package catalog/inventaire
     coordo add-foreignkey ind.ess_arb dens_bois.ess_arb --package catalog/inventaire
     coordo load kobotoolbox data/20240808_EnqueteMenage_CDF_QuestionnaireK.xlsx data/20241007_EnqueteMenage_CDF_Donnees.csv --package catalog/enquete
+    coordo load file data/socio_eco_gps.csv --package catalog/enquete
+    coordo add-foreignkey enquete_menage_cdf.admi2 socio_eco_gps.admi2 --package catalog/enquete
 fi
 
 if [ -d "static" ]; then
