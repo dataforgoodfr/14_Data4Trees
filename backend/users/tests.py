6be06c37a3fcc9b822c12e41e5f3149d0dd90f1f
Olivier Coen <coen.olivier@gmail.com>
2026-04-05 11:27:21 +0200

    add unit test

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,20 @@
-# from django.test import TestCase
+from django.test import TestCase
+from .models import CustomUser
+from django.contrib.auth.models import Permission
 
-# Create your tests here.
+
+class UserPermissionTest(TestCase):
+
+    def setUp(self):
+        self.user_1 = CustomUser.objects.create_user(username='testuser1', password='testpass1')
+        self.user_2 = CustomUser.objects.create_user(username='testuser2', password='testpass2')
+        permission = Permission.objects.get(codename='add_data')
+        # adding permission to both users
+        self.user_1.user_permissions.add(permission)
+        self.user_2.user_permissions.add(permission)
+        # removing permission from user_2
+        self.user_2.user_permissions.remove(permission)
+
+    def test_user_can_data(self):
+        self.assertTrue(self.user_1.has_perm('users.add_data'))
+        self.assertFalse(self.user_2.has_perm('users.add_data'))
