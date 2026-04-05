0cd32869cee7a21260ff1e3e57a2ec91c0d89e12
Olivier Coen <coen.olivier@gmail.com>
2026-04-05 11:21:15 +0200

    clean admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,12 +5,9 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + (
-        'is_active',
-        'is_superuser',
-        'can_add_data'
-    )
+    list_display = UserAdmin.list_display + ('can_add_data',)
 
     @admin.display(boolean=True, description='Can add data')
-    def can_add_data(self, obj):
-        return obj.has_perm('add_data')
+    def can_add_data(self, obj: CustomUser):
+        # NOTE: the has_perm method returns True for all active superusers, no matter what other permissions are assigned
+        return obj.has_perm('users.add_data')
