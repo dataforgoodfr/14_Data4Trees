5355201e02a6b8c8acc41a6baf208f119efcff76
Olivier <46594602+OlivierCoen@users.noreply.github.com>
2026-04-06 10:54:27 +0200

    Update backend/users/admin.py
    Co-authored-by: Arnaud Fournier <arnaudfournier921@gmail.com>

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -5,7 +5,15 @@ from .models import CustomUser
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
-    list_display = UserAdmin.list_display + ('can_add_data',)
+    list_display = UserAdmin.list_display + ('can_add_data',) + ('project',)
+
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional Info", {"fields": ("project",)}),
+    )
+
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional Info", {"classes": ("wide",), "fields": ("project",)}),
+    )
 
     @admin.display(boolean=True, description='Can add data')
     def can_add_data(self, obj: CustomUser):
