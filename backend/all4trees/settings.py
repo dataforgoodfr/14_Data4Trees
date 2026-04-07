0cb57759cfb80b951bbb8cea6ace8bdaf5d27af2
Olivier <46594602+OlivierCoen@users.noreply.github.com>
2026-04-07 20:06:33 +0200

    Feat/add other staging env (#127)

--- backend/all4trees/settings.py
+++ backend/all4trees/settings.py
@@ -20,74 +20,79 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ot7b#&pes@%&0b2w(e@b278k2x2n4y$1@t4(!@(c&zthr3k)n('
+SECRET_KEY = "django-insecure-ot7b#&pes@%&0b2w(e@b278k2x2n4y$1@t4(!@(c&zthr3k)n("
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', 'data4trees.services.d4g.fr']
+ALLOWED_HOSTS = [
+    "localhost",
+    "test-data4trees.services.d4g.fr",
+    "data4trees.services.d4g.fr",
+]
 
 CSRF_TRUSTED_ORIGINS = [
+    "https://test-data4trees.services.d4g.fr",
     "https://data4trees.services.d4g.fr",
 ]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic', # see https://whitenoise.readthedocs.io/en/stable/django.html#using-whitenoise-in-development
-    'users',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'rest_framework_simplejwt',
+    "whitenoise.runserver_nostatic",  # see https://whitenoise.readthedocs.io/en/stable/django.html#using-whitenoise-in-development
+    "users",
+    "django.contrib.admin",
+    "django.contrib.admindocs",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "rest_framework_simplejwt",
     "corsheaders",
     "maps",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-ROOT_URLCONF = 'all4trees.urls'
+ROOT_URLCONF = "all4trees.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'all4trees.wsgi.application'
+WSGI_APPLICATION = "all4trees.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db" / "db.sqlite3",
     }
 }
 
@@ -97,44 +102,39 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
-AUTH_USER_MODEL = 'users.CustomUser'
+AUTH_USER_MODEL = "users.CustomUser"
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    "https://test-data4trees.services.d4g.fr",
     "https://data4trees.services.d4g.fr",
 ]
 
 # Enable credentials (e.g., cookies) if needed
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_METHODS = (
-    "GET",
-    "POST",
-    "PUT",
-    "PATCH",
-    "DELETE"
-)
+CORS_ALLOW_METHODS = ("GET", "POST", "PUT", "PATCH", "DELETE")
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -144,19 +144,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / STATIC_URL
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
 }
 
 STORAGES = {
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage", # see https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",  # see https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
     },
 }
--- docker/README.md
+++ docker/README.md
@@ -22,32 +22,53 @@ docker compose -f docker/compose.yaml -f docker/compose.dev.yaml up
 
 Vous pouvez accéder à l'application en tapant `http://localhost:5173/` dans la barre d'adresse de votre navigateur.
 
->[!TIP]
->Pour forcer le build complet des applications (si vous ajoutez une dépendance par exemple):
->```bash
->docker compose -f docker/compose.yaml -f docker/compose.dev.yaml up --build
->```
->
->Pour ne lancer que le backend:
->```bash
->docker compose -f docker/compose.yaml -f docker/compose.dev.yaml up backend
->```
-
 >[!IMPORTANT]
 >Si vous voulez vous connecter en tant qu'admin à l'application locale, le nom d'utilisateur ainsi que le mot de passe sont accessibles [ici](./compose.dev.yaml#L8).
 
+## Tips
 
-## Environnement de staging
+Pour forcer le build complet des applications (si vous ajoutez une dépendance par exemple):
+```bash
+docker compose -f docker/compose.yaml -f docker/compose.dev.yaml up --build
+```
 
-### Déploiement
+Pour ne lancer que le backend:
+```bash
+docker compose -f docker/compose.yaml -f docker/compose.dev.yaml up backend
+```
 
-Data4Good utilise [Coolify](https://coolify.io/) pour le déploiement de ses applications en mode staging. Si vous avez un compte sur le Coolify de Data4Good, vous pouvez accéder à la [configuration de l'application de staging de Data4Trees](https://coolify.services.d4g.fr/project/j00ocs0s48wcwsos4so04kck/environment/rkw0kk88808wow0sow0owc8w/application/d00kogcs8kggc088wk0ss8ks).
+>[!NOTE]
+>`docker` garde un build cache afin de ne pas avoir à tout télécharger chaque chaque fois et ainsi rendre le `build` plus rapide.
+>De ce fait, si des changements ont été fait côté `coordo`, il se pourrait que `docker` ne les télécharge pas automatiquement.
+>Dans ce cas, vous pouvez forcer le `build` complet avec:
+>```bash
+>docker compose -f docker/compose.dev.yaml build --no-cache
+>```
+>et relancer l'application
+>Si vous avez toujours un problème, vous pouvez tenter de supprimer les containers / images et volumes avec:
+>```
+>docker system prune -a --volumes
+>```
+
+
+## Environnement de test / staging
+
+Data4Good utilise [Coolify](https://coolify.io/) pour le déploiement de ses applications en mode test / staging. Si vous avez un compte sur le Coolify de Data4Good, vous pouvez accéder à la [configuration de l'application de staging de Data4Trees](https://coolify.services.d4g.fr/project/j00ocs0s48wcwsos4so04kck/environment/rkw0kk88808wow0sow0owc8w/application/d00kogcs8kggc088wk0ss8ks).
 
 >[!NOTE]
 >Si vous avez besoin de tester votre code sur l'environnement staging, vous pouvez contacter @RonanMorgan pour qu'il vous crée un compte.
 
+### Déploiement en test
+
+En premier lieu, pour chaque PR, il convient de déployer le code de la branche sur l'environnement de test.
+Le déploiement de staging va utiliser les fichiers `docker/compose.yaml` et `docker/compose.test.yaml`.
+L'application de staging est disponible à l'adresse: [https://test-data4trees.services.d4g.fr/](https://test-data4trees.services.d4g.fr/).
+
+
+### Déploiement en staging
+
+Pour les asociations, une vraie application de staging est dispo à l'adresse [https://data4trees.services.d4g.fr/](https://data4trees.services.d4g.fr/)
 Le déploiement de staging va utiliser les fichiers `docker/compose.yaml` et `docker/compose.staging.yaml`.
-L'application de staging est disponible à l'adresse: [https://data4trees.services.d4g.fr/](https://data4trees.services.d4g.fr/).
 
 ### Session admin
 
