91b087ae2db45a882fc2f6147a0e7c8689f01e31
Arnaud Fournier <arnaudfournier921@gmail.com>
2026-04-22 18:07:22 +0200

    Feat(Dashboard): Add route that returns aggregated dashboard data

--- backend/maps/views.py
+++ backend/maps/views.py
@@ -2,6 +2,7 @@ from coordo.map import Map
 from django.conf import settings
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import pandas as pd
 
 config_path = settings.BASE_DIR / "configs" / "config.json"
 map = Map.from_file(config_path)
@@ -15,3 +16,22 @@ def my_map_view(request, subpath):
             request.body,
         )
     )
+
+def dashboard_view(request, layer_id):
+    data = map.handle_request(
+            'POST',
+            layer_id,
+            request.body)
+
+    df = pd.DataFrame([feat.get("properties", {}) for feat in data["features"]])
+
+    # keys we want to drop from the dataframe before converting to numeric, as they are not relevant for the statistics
+    fieldToDrop = ["for", "cod"]
+
+    numeric = df.drop(columns=fieldToDrop, errors="ignore").apply(pd.to_numeric, errors="coerce")
+    stats = pd.DataFrame({
+        "mean": numeric.mean(numeric_only=True).round(2),
+        "std": numeric.std(numeric_only=True).round(2),
+    })
+    return JsonResponse(stats.to_dict())
+
