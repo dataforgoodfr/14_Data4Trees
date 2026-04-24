65fe88bf54c5e557edac0f81892fbd4bd654a0cf
Arthur-Henri Michalland <ah.michalland@gmail.com>
2026-04-24 17:42:22 +0200

    fix: changed header right button to navigate either to dashboard or to home

--- webapp/src/widgets/header/index.tsx
+++ webapp/src/widgets/header/index.tsx
@@ -3,11 +3,22 @@ import { DashboardPopover } from "@widgets/DashboardPopover";
 import { UserMenu } from "./user-menu";
 import { Button } from "@shared/ui/button";
 import { URLS } from "@shared/urls";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useTranslation } from "@shared/i18n";
 
 export function Header() {
   const navigate = useNavigate();
-  const navDashboard = () => navigate(URLS.DASHBOARD);
+  const location = useLocation();
+  const path = location.pathname;
+  const { t } = useTranslation("translations");
+
+  function navDashboard() {
+    if (path === "/") {
+      navigate(URLS.DASHBOARD);
+    } else if (path === "/dashboard") {
+      navigate(URLS.HOME)
+    }
+  }
   return (
     <header className="bg-background border-b border-border p-3 relative z-40 flex-0">
       <div className="mx-auto max-w-screen-2xl">
@@ -19,7 +30,11 @@ export function Header() {
           />
 
           <div className="flex items-center gap-3">
-             <Button variant="default" onClick={navDashboard}>Montrer Dashboard</Button>
+             <Button variant="default" onClick={navDashboard}>
+              {(path === "/") ?
+                <p>{t("buttonHeader.toDashboard")}</p> :
+                <p>{t("buttonHeader.toHome")}</p>}
+             </Button>
             <UserMenu />
           </div>
         </div>
