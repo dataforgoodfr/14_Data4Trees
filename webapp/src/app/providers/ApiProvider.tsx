7cf202960943ff9174a068a03b92738f55d338e0
Arthur-Henri Michalland <ah.michalland@gmail.com>
2026-04-24 16:16:39 +0200

    feat: dashboard init / création page - route - début API

--- webapp/src/app/providers/ApiProvider.tsx
+++ webapp/src/app/providers/ApiProvider.tsx
@@ -2,13 +2,15 @@ import { type ReactNode, useMemo } from "react";
 
 import { createApiClient } from "@shared/api/client";
 import { ApiContext } from "@shared/contexts/ApiContext";
+import { useAuth } from "@features/auth/useAuth";
 
 interface ApiProviderProps {
   children: ReactNode;
 }
 
 export function ApiProvider({ children }: ApiProviderProps) {
-  const api = useMemo(() => createApiClient(), []);
+  const {token} = useAuth()
+  const api = useMemo(() => createApiClient(token), []);
 
   return <ApiContext value={api}>{children}</ApiContext>;
 }
