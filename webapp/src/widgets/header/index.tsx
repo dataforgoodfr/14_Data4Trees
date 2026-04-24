aaa30f6f2967fcf77120e5b8a5a0791c99879c9e
Arthur-Henri Michalland <ah.michalland@gmail.com>
2026-04-24 16:16:39 +0200

    feat: dashboard init / création page - route - début API

--- webapp/src/widgets/header/index.tsx
+++ webapp/src/widgets/header/index.tsx
@@ -1,8 +1,13 @@
 import { DashboardPopover } from "@widgets/DashboardPopover";
 
 import { UserMenu } from "./user-menu";
+import { Button } from "@shared/ui/button";
+import { URLS } from "@shared/urls";
+import { useNavigate } from "react-router-dom";
 
 export function Header() {
+  const navigate = useNavigate();
+  const navDashboard = () => navigate(URLS.DASHBOARD);
   return (
     <header className="bg-background border-b border-border p-3 relative z-40 flex-0">
       <div className="mx-auto max-w-screen-2xl">
@@ -14,7 +19,7 @@ export function Header() {
           />
 
           <div className="flex items-center gap-3">
-            <DashboardPopover dataType="example" />
+             <Button variant="default" onClick={navDashboard}>Montrer Dashboard</Button>
             <UserMenu />
           </div>
         </div>
