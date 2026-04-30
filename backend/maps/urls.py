c0b4593e716bd51d0cb617dfb73a597244d42533
Olivier <coen.olivier@gmail.com>
2026-04-30 14:01:02 +0200

    add view

--- backend/maps/urls.py
+++ backend/maps/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    path("add-data/", views.add_data_view, name="maps-add-data"),
     re_path(r"^(?!dashboard)(?P<subpath>\w+)", views.my_map_view, name="maps-data"),
     path("dashboard/<layer_id>", views.dashboard_view, name="dashboard-data"),
 ]
