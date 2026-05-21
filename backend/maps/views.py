d530842c2d6fbfd1c96c7b385adef7d1230527b5
NicoPL <22911491+NicoPL@users.noreply.github.com>
2026-05-21 15:13:37 +0200

    Use external data for clustering & change json format

--- backend/maps/views.py
+++ backend/maps/views.py
@@ -12,6 +12,8 @@ import pandas as pd
 import numpy as np
 import chardet
 
+from . import stats
+
 config_path = settings.BASE_DIR / "configs" / "config.json"
 map = Map.from_file(config_path)
 
@@ -25,20 +27,6 @@ def my_map_view(request, subpath):
         )
     )
 
-def weighted_mean_std(series, weights):
-    mask = series.notna() & weights.notna()
-    x = series[mask].astype(float)
-    w = weights[mask].astype(float)
-
-    if len(x) == 0:
-        return {"value": None, "error": None}
-
-    mean = np.average(x, weights=w)
-    variance = np.average((x - mean) ** 2, weights=w)
-    std = np.sqrt(variance)
-
-    return {"value": mean, "error": std}
-
 def dashboard_view(request, layer_id):
     data = map.handle_request(
             'POST',
@@ -46,18 +34,8 @@ def dashboard_view(request, layer_id):
             request.body)
 
     if (layer_id == "inventaire"):
-        df = pd.DataFrame([feat.get("properties", {}) for feat in data["features"]])
 
-        weights_map = {"1": 1092, "2": 752, "3": 775, "4": 41}
-        df["weight"] = df["for"].map(weights_map)
-
-        fields_to_drop = ["for", "cod", "weight"]
-        numeric = df.drop(columns=fields_to_drop, errors="ignore").apply(pd.to_numeric, errors="coerce")
-
-        result = {
-            col: weighted_mean_std(numeric[col], df["weight"])
-            for col in numeric.columns
-        }
+        result = stats.compute_aggregation(data)
 
         return JsonResponse(result)
     
