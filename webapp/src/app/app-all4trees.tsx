3ec1ba92ad3baf96c51e4b63ab085e3edbf722bd
David Bretaud <david.bretaud.dev@gmail.com>
2026-06-15 23:18:37 +0200

    fix(tailwind): Refine how css is loaded to avoid duplicate

--- webapp/src/app/app-all4trees.tsx
+++ webapp/src/app/app-all4trees.tsx
@@ -2,7 +2,6 @@ import { AppWrapper } from "./app-wrapper";
 import { AppRouterAll4Trees } from "./routes/app-router-all4trees";
 
 import "./styles/globals.css";
-import "./styles/index.css";
 import "./styles/all4trees.css";
 
 const AppAll4Trees = () => {
