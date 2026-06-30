e6573d181f7ec1fa7efe07114acc2dad0da64740
Sylvain Lesage <severo@rednegra.net>
2026-06-30 16:08:56 +0200

    fix fetch logic and error management

--- webapp/src/pages/all4trees/dashboard.tsx
+++ webapp/src/pages/all4trees/dashboard.tsx
@@ -1,20 +1,5 @@
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
-
 import Dashboard from "@widgets/dashboard/dashboard";
-import { getFallbackRender } from "@widgets/dashboard/error-boundary-fallback";
-import Loading from "@widgets/dashboard/loading";
-
-import { useTranslation } from "@shared/i18n";
 
 export default function DashboardPage() {
-  const { t } = useTranslation("all4trees");
-
-  return (
-    <ErrorBoundary fallbackRender={getFallbackRender({ t })}>
-      <Suspense fallback={<Loading />}>
-        <Dashboard />
-      </Suspense>
-    </ErrorBoundary>
-  );
+  return <Dashboard />;
 }
--- webapp/src/widgets/dashboard/error-boundary-fallback.tsx
+++ webapp/src/widgets/dashboard/error-boundary-fallback.tsx
@@ -3,8 +3,10 @@ import { type FallbackProps, getErrorMessage } from "react-error-boundary";
 
 // t must be passed to the fallback render function because the error boundary fallback component cannot call hooks like useTranslation
 export function getFallbackRender({
+  retry,
   t,
 }: {
+  retry?: () => void;
   t: TFunction<"all4trees", undefined>;
 }) {
   function FallbackRender({ error }: FallbackProps) {
@@ -13,10 +15,19 @@ export function getFallbackRender({
 
     return (
       <div className="flex flex-col items-center pt-24 gap-4">
-        <h1 className="text-2xl font-bold text-destructive">
+        <h1 className="text-2xl font-bold text-accent">
           {t("dashboard.error.title")}
         </h1>
         <p className="mt-2">{errorMessage}</p>
+        {retry && (
+          <button
+            className="mt-4 border border-accent rounded px-4 py-2 bg-primary hover:bg-accent text-xl hover:cursor-pointer"
+            onClick={retry}
+            type="button"
+          >
+            {t("dashboard.error.retry")}
+          </button>
+        )}
       </div>
     );
   }
