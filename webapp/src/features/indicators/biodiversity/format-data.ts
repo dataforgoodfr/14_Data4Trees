89420074c85ce6f06708465a922bbed1d0a4442e
Arnaud Fournier <arnaudfournier921@gmail.com>
2026-07-01 12:58:17 +0200

    Fix(indicators): Fix taxon abundances

--- webapp/src/features/indicators/biodiversity/format-data.ts
+++ webapp/src/features/indicators/biodiversity/format-data.ts
@@ -11,6 +11,7 @@ import type { ForestInventoryData } from "@features/popup/forest-inventory/types
 import type { NumericKeys } from "@shared/types";
 
 import type { BiodiversityData } from "./types";
+import type { ForestInventoryData } from "@features/popup/forest-inventory/types";
 
 const indicatorKeys: NumericKeys<BiodiversityData>[] = [
   "biomass_volume",
