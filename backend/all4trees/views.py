9ac415a54e7c7943cdba74f5428126841428c684
Arnaud Fournier <arnaudfournier921@gmail.com>
2026-07-04 12:00:56 +0200

    remove unused import

--- backend/all4trees/views.py
+++ backend/all4trees/views.py
@@ -1,11 +1,11 @@
 from django.conf import settings
-from django.contrib.auth.decorators import permission_required
-import pandas as pd
-import json
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
+import pandas as pd
+import json
 
 catalog_path = settings.BASE_DIR / "catalog"
 
