b9f0490a873b2fbf16883fa1ef3308ce26fda594
David Bretaud <david.bretaud.dev@gmail.com>
2026-08-08 11:26:03 +0200

    feat(views/filters): Add loc1 filter as well for multi filtering

--- backend/maps/services/filters.py
+++ backend/maps/services/filters.py
@@ -13,13 +13,19 @@ def get_all4trees_filters(user_map):
     layer_data_inventaire_bio = get_layer_data_properties(layer_inventaire_bio)
     layer_data_enquete = get_layer_data_properties(layer_enquete)
     
+    # Compute filters values
     project_values = get_project_values(
-    layer_data_inventaire_for=layer_data_inventaire_for,
-    layer_data_inventaire_bio=layer_data_inventaire_bio,
+        layer_data_inventaire_for=layer_data_inventaire_for,
+        layer_data_inventaire_bio=layer_data_inventaire_bio,
+    )
+    loc1_values = get_loc1_values(
+        layer_data_inventaire_for=layer_data_inventaire_for,
+        layer_data_inventaire_bio=layer_data_inventaire_bio,
     )
     
     return {
-        "project": project_values
+        "project": project_values,
+        "loc1": loc1_values,
     }
     
 def get_layer(user_map, layer_id):
@@ -44,5 +50,21 @@ def get_project_values(
         LAYER_INVENTAIRE_BIO: {
             "property_name": "project",
             "values": list(set([item["project"] for item in layer_data_inventaire_bio])),
+        },
+    }
+
+def get_loc1_values(
+    layer_data_inventaire_for,
+    layer_data_inventaire_bio,
+):
+    """Retrieve loc1 property on layers where this field exist"""
+    return {
+        LAYER_INVENTAIRE_FOR: {
+            "property_name": "loc1",
+            "values": list(set([item["loc1"] for item in layer_data_inventaire_for])),
+        },
+        LAYER_INVENTAIRE_BIO: {
+            "property_name": "loc1",
+            "values": list(set([item["loc1"] for item in layer_data_inventaire_bio])),
         },
     }
