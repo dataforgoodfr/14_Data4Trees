3e61d848be721b87f2a4a04dd997a2c5bdcf41dc
David Bretaud <david.bretaud.dev@gmail.com>
2026-09-13 12:05:29 +0200

    fix(backend): fix typo in typing

--- backend/maps/services/filters.py
+++ backend/maps/services/filters.py
@@ -1,4 +1,4 @@
-from ..constants import LAYER_INVENTAIRE_FOR, LAYER_INVENTAIRE_BIO, LAYER_ENQUETE
+from ..constants import LAYER_INVENTAIRE_FOR, LAYER_INVENTAIRE_BIO
 
 ########## FILTERS ENTRYPOINT ##########
  
@@ -6,12 +6,12 @@ def get_all4trees_filters(user_map):
     # Retrieve all layers data
     layer_inventaire_for = get_layer(user_map, LAYER_INVENTAIRE_FOR)
     layer_inventaire_bio = get_layer(user_map, LAYER_INVENTAIRE_BIO)
-    layer_enquete = get_layer(user_map, LAYER_ENQUETE)
+    # layer_enquete = get_layer(user_map, LAYER_ENQUETE)
     
     # Retrieve the data points properties
     layer_data_inventaire_for = get_layer_data_properties(layer_inventaire_for)
     layer_data_inventaire_bio = get_layer_data_properties(layer_inventaire_bio)
-    layer_data_enquete = get_layer_data_properties(layer_enquete)
+    # layer_data_enquete = get_layer_data_properties(layer_enquete)
     
     # Compute filters values
     project_values = get_project_values(
@@ -31,7 +31,7 @@ def get_all4trees_filters(user_map):
 def get_layer(user_map, layer_id):
     return user_map.handle_request(method='POST',path=layer_id, filters={})
 
-def get_layer_data_properties(layer)-> list(dict):
+def get_layer_data_properties(layer) -> list[dict]:
     features =  layer["features"]
     return [feat["properties"] for feat in features]
 
